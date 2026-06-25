80fa0c6eac0c7db9ed44f7e01837dc3b209bb9a8
Enrique Zavala <enrique.zavala@tecdesoftware.edu.mx>
2026-06-24 19:57:56 -0600

    feat: add MaxLength and Required annotations to Task

--- TaskFlow.Domain/Models/Task.cs
+++ TaskFlow.Domain/Models/Task.cs
@@ -1,15 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskFlow.Domain.Models
 {
     public class Task
     {
         public int Id { get; set; }
         public int ActivityId { get; set; }
+
+        [Required] [MaxLength(100)]
         public string Title { get; set; } = string.Empty;
+
+        [MaxLength(500)]
         public string Description { get; set; } = string.Empty;
+
         public bool IsCompleted { get; set; } = false;
         public DateTime? DueTime { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        
+
         public Activity Activity { get; set; } = null!;
     }
 }
